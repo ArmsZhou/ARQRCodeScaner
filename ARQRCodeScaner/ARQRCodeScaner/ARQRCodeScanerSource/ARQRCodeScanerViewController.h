bda8a3b5ce30907ecfc5e6e688322a24c725871f
zhoudl <zhoudl1990@gmail.com>
2015-11-10 18:09:47 +0800

    new class

---
+++ ARQRCodeScaner/ARQRCodeScaner/ARQRCodeScanerSource/ARQRCodeScanerViewController.h
@@ -0,0 +1,15 @@
+//
+//  ARQRCodeScanerViewController.h
+//  ARQRCodeScaner
+//
+//  Created by zhoudl on 15/11/10.
+//  Copyright © 2015年 zhoudl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+@interface ARQRCodeScanerViewController : UIViewController
+
+@end
---
+++ ARQRCodeScaner/ARQRCodeScaner/ARQRCodeScanerSource/ARQRCodeScanerViewController.m
@@ -0,0 +1,29 @@
+//
+//  ARQRCodeScanerViewController.m
+//  ARQRCodeScaner
+//
+//  Created by zhoudl on 15/11/10.
+//  Copyright © 2015年 zhoudl. All rights reserved.
+//
+
+#import "ARQRCodeScanerViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import <AudioToolbox/AudioToolbox.h>
+
+@interface ARQRCodeScanerViewController ()
+
+@end
+
+@implementation ARQRCodeScanerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
